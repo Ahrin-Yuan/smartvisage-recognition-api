8a907ff0060d9d688983d369227d79c08c3dc621
Ahrin <hinatairene99@gmail.com>
2025-06-14 02:01:01 +0800

    update port env var

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const port = process.env.PORT || 8000
 
 const db = knex({ 
   client: 'pg',
@@ -43,6 +44,7 @@ app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req,res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-    console.log('app is running on port 3000');
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`)
 })
